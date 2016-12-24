91321943ceb291ff1055dd5fb4886b1cbaf397ab
sergii-tsymbaliuk <ser9ey@gmail.com>
2016-12-23 23:08:49 -0800

    Sparse arrays task  -  hackerrank

---
+++ src/SparseArrays.java
@@ -0,0 +1,47 @@
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+/**
+ * Created by stsym on 12/23/2016.
+ */
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner=new Scanner(System.in);
+        LinkedHashMap<String, Integer> words = new LinkedHashMap<>();
+        int numWords = scanner.nextInt();
+        scanner.nextLine();
+        for (int i=0; i<numWords; i++){
+            String word = scanner.nextLine();
+            if (words.keySet().contains(word)){
+                words.put(word, words.get(word)+1);
+            } else {
+                words.put(word, 1);
+            }
+        }
+
+        LinkedHashMap<String, Integer> queries = new LinkedHashMap<>();
+        int numQueries = scanner.nextInt();
+        scanner.nextLine();
+        for (int i=0; i<numQueries; i++){
+            String query = scanner.nextLine();
+            if (words.keySet().contains(query)){
+                queries.put(query, words.get(query));
+            } else {
+                queries.put(query, 0);
+            }
+        }
+
+        queries.forEach((s,n)-> System.out.println(n));
+    }
+}
+
+public class SparseArrays {
+
+    public static void main(String [] args){
+        Solution.main(args);
+    }
+}
