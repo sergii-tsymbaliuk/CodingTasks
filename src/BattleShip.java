135a288df48f6574533402e278cbacd0732f2263
Sergii Tsymbaliuk <ser9ey@gmail.com>
2017-11-13 23:24:40 -0800

    Revised versions to improve performance

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -4,69 +4,51 @@ import java.util.HashSet;
  * Created by stsym on 12/12/2016.
  */
 public class BattleShip {
-    public static class Solution {
-        public int countBattleships(char[][] board) {
-            boolean vertical = false;
-            HashSet<Integer> verticals = new HashSet<>();
-            int count=0;
-            for (int i=0; i<board.length; i++){
-                boolean horisontal = false;
-                for (int j=0; j<board[i].length; j++){
 
-                    if (board[i][j] == 'X' && !horisontal && !verticals.contains(j)){
-                        System.out.println("X["+i+","+j+"]");
+  public static class Solution {
 
-                        if (i==board.length-1 || board[i+1][j] != 'X'){
-                            System.out.println("1111");
-                            if (j==board[i].length-1 || board[i][j+1] != 'X'){
-                                System.out.println("Count ["+i+","+j+"]:"+count);
-                                count++;
-                                System.out.println("2222");
-                                continue;
-                            } else {
-                                horisontal = true;
-                                continue;
-                            }
-                        } else {
-                            verticals.add(j);
-                        }
-                    }
-
-                    if (verticals.contains(j)){
-                        if (board[i][j] != 'X' || i == board.length-1){
-                            verticals.remove(j);
-                            System.out.println("vert: ["+i+","+j+"] count="+count);
-                            count++;
-                        } else {
-                            System.out.println("WTF vertical, ["+i+","+j+"], count="+count+"?");
-                        }
-                    }
-
-                    if (horisontal) {
-                        if (board[i][j] != 'X' || j == board[i].length - 1) {
-                            horisontal = false;
-                            System.out.println("hor: ["+i+","+j+"] count="+count);
-                            count++;
-                        } else {
-                            System.out.println("WTF horisontal, ["+i+","+j+"], count="+count+"?");
-                        }
-                    }
-
-                }
-            }
-            return count;
+    public int countBattleships(char[][] board) {
+      int count = 0;
+      for (int i = 0; i < board.length - 1; i++) {
+        for (int j = 0; j < board[i].length - 1; j++) {
+          if (board[i][j] == 'X' && board[i + 1][j] == '.' && board[i][j + 1] == '.') {
+            count++;
+          }
         }
-    }
-    public static void main(String [] args){
-        char [][] board  = {
-                {'X','.','.','.','X'},
-                {'X','.','.','.','X'},
-                {'.','.','.','.','X'},
-                {'X','.','X','X','.'},
-                {'X','.','.','.','X'}
-        };
-        Solution solution = new Solution();
-        System.out.println(solution.countBattleships(board));
+      }
 
-    }
+      int last_i = board.length - 1;
+      for (int j = 0; j < board[last_i].length - 1; j++) {
+        if (board[last_i][j] == 'X' && board[last_i][j + 1] == '.') {
+          count++;
+        }
+      }
+
+      int last_j = board[last_i].length - 1;
+      for (int i = 0; i < board.length - 1; i++) {
+        if (board[i][last_j] == 'X' && board[i + 1][last_j] == '.') {
+          count++;
+        }
+      }
+      if (board[last_i][last_j] == 'X') {
+        count++;
+      }
+
+    return count;
+  }
+
+}
+
+  public static void main(String[] args) {
+    char[][] board = {
+        {'X', '.', '.', '.', 'X'},
+        {'X', '.', '.', '.', 'X'},
+        {'.', '.', '.', '.', 'X'},
+        {'X', '.', 'X', 'X', '.'},
+        {'X', '.', '.', '.', 'X'}
+    };
+    Solution solution = new Solution();
+    System.out.println(solution.countBattleships(board));
+
+  }
 }
--- src/LongestSubstrNoRepChar.java
+++ src/LongestSubstrNoRepChar.java
@@ -1,7 +1,33 @@
+import java.util.HashSet;
+
 /**
  * Created by stsym on 12/5/2016.
  */
 public class LongestSubstrNoRepChar {
+    public class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s.length() < 2)
+                return s.length();
+
+            String maximum = s.substring(0,1);
+
+            for (int i=0; i<s.length(); i++){
+                HashSet<Character> chars = new HashSet<>();
+                chars.add(s.charAt(i));
+
+                for (int j=i+1; j<s.length() && !chars.contains(s.charAt(j)); j++){
+                    chars.add(s.charAt(j));
+                    if (j-i+1 > maximum.length()){
+                        maximum = s.substring(i, j+1);
+                    }
+                }
+
+            }
+
+            return maximum.length();
+        }
+    }
+
     public static void main(String [] args){
 
     }
