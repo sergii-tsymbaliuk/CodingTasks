f7e905d7b6afaa172bc530157081ae49140a1023
Sergii Tsymbaliuk <sergii@tsymbaliuk.net>
2019-02-17 23:40:17 +0000

    Add AppleStocks coding task

---
+++ src/test/java/InterviewCake/AppleStocks/AppleStocksTest.java
@@ -0,0 +1,12 @@
+package InterviewCake.AppleStocks;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppleStocksTest {
+  @Test
+  void testSolution() {
+    AppleStocks as = new AppleStocks(new int[]{10, 7, 5, 8, 11, 9});
+    assertEquals(6, as.solution());
+  } 
+}
